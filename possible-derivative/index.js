285da1c281b81504596274f00ddf9021d2e9ce1a
Kyle Mitchell <kyle@kemitchell.com>
2015-04-26 19:37:06 -0700

    add comment at dynamic modification

--- possible-derivative/index.js
+++ possible-derivative/index.js
@@ -1,5 +1,8 @@
 var underlying = require('underlying');
 
+// NOTE: This module makes a change, at runtime,
+// to logic defined by the other module.
+
 underlying.secondary = function(argument) {
   return argument.toLowerCase();
 };
